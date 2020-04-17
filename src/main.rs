9d1bd7f1f9c02d23d1506efef752dfe96d7c302f
ArthurMatthys <amatthys@student.le-101.fr>
2020-04-17 09:45:17 +0200

    test compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
+
 fn main() {
-    println!("Hello World");
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+    MessageDialog::new(
+        None::<&Window>,
+        DialogFlags::empty(),
+        MessageType::Info,
+        ButtonsType::Ok,
+        "Hello World",
+    )
+    .run();
 }
